516805021d9d73056bb9bba027eb6bec7becee6a
Juanky <JuanC.Prieto.Silos@gmail.com>
2017-12-11 23:57:50 +0100

    Error a crear la carpeta de usuario

--- src/main/java/es/jklabs/utilidades/UtilidadesConfiguracion.java
+++ src/main/java/es/jklabs/utilidades/UtilidadesConfiguracion.java
@@ -53,12 +53,7 @@ public class UtilidadesConfiguracion {
         ObjectMapper mapper = new ObjectMapper();
         mapper.enable(SerializationFeature.INDENT_OUTPUT);
         try {
-            if (!new File(UtilidadesFichero.HOME + UtilidadesFichero.SEPARADOR +
-                    UtilidadesFichero.BEYOND_DATA_BASE_TRANSFER_FOLDER).exists()) {
-                File carpeta = new File(UtilidadesFichero.HOME + UtilidadesFichero.SEPARADOR +
-                        UtilidadesFichero.BEYOND_DATA_BASE_TRANSFER_FOLDER);
-                carpeta.mkdir();
-            }
+            UtilidadesFichero.createBaseFolder();
             mapper.writeValue(new File(UtilidadesFichero.HOME + UtilidadesFichero.SEPARADOR +
                     UtilidadesFichero.BEYOND_DATA_BASE_TRANSFER_FOLDER + UtilidadesFichero.SEPARADOR + CONFIG_JSON), configuracion);
         } catch (IOException e) {
